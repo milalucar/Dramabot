1602a7386d08b5886f208bcd1a626d2d6e08402d
milalucar <37478294+milalucar@users.noreply.github.com>
2018-03-17 18:09:25 +0100

    Update drama.js

--- script/commands/drama/drama.js
+++ script/commands/drama/drama.js
@@ -1,19 +1,19 @@
 var listDrama1 = new dramaList();
 
-new drama()
-    .setName("Test")
-    .setImage("https://tse2.mm.bing.net/th?id=OIP.NhJdBlIKL2rNrjnRIh5cqAHaHa&pid=15.1&P=0&w=300&h=300")
-    .setStory_fr("Histoire")
-    .setStory_fr_source("undefined")
-    .setStory_en("Story")
-    .setStory_en_source("undefined")
-    .setDrama_fr("undefined")
-    .setDrama_en("undefined")
-    .setOp([])
-    .setOpFull([])
-    .setEd([])
-    .setEdFull([])
-    .addTo(listDrama1);
+var drm = new drama()
+    drm.setName("Test")
+    drm.setImage("https://tse2.mm.bing.net/th?id=OIP.NhJdBlIKL2rNrjnRIh5cqAHaHa&pid=15.1&P=0&w=300&h=300")
+    drm.setStory_fr("Histoire")
+    drm.setStory_fr_source("undefined")
+    drm.setStory_en("Story")
+    drm.setStory_en_source("undefined")
+    drm.setDrama_fr("undefined")
+    drm.setDrama_en("undefined")
+    drm.setOp([])
+    drm.setOpFull([])
+    drm.setEd([])
+    drm.setEdFull([])
+    drm.addTo(listDrama1);
 
     
 exports.load = () => {
