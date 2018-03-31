d4b3e93a8492d9ff7079928cc805fc3540396af6
milalucar <37478294+milalucar@users.noreply.github.com>
2018-03-31 12:11:18 +0200

    Update drama.js

--- script/commands/drama/drama.js
+++ script/commands/drama/drama.js
@@ -1,5 +1,6 @@
 var listDrama1 = new dramaList();
 
+/*
 new drama()
     .setName("Test")
     .setImage("https://tse2.mm.bing.net/th?id=OIP.NhJdBlIKL2rNrjnRIh5cqAHaHa&pid=15.1&P=0&w=300&h=300")
@@ -14,8 +15,109 @@ new drama()
     .setEd(["undefined"])
     .setEdFull(["undefined"])
     .addTo(listDrama1);
+*/
+new drama()
+   .setName("The légende of blue sea")
+   .setImage("https://www.nautiljon.com/images/galerie/11/54/legend_of_the_blue_sea_885345.jpg")
+   .setStory_fr("Shim Cheong (Jun Ji Hyun), une des dernières sirène vivante se retrouve projetée sur terre après un raz-de-marée. Elle rencontre Heo Joon Jae (Lee Min Oh), un escroc charmant qui est le sosie de Kim Dam Ryeong, un noble de Joseon. Dam Ryeong, magistrat de Heupgok Hyun au 16e siècle, a sauvé une sirène des mains d'un marchand qui voulait l'exploiter. Dans les temps présents, Heo Joon Jae est un arnaqueur qui travaille avec Jo Nam Doo (Lee Hee Joon) et Tae Oh (Shin Won Ho), deux autres escrocs. Leur principal occupation est d'escroquer les riches pour gagner le plus d'argent possible. L'amie de Joon Jae, Cha Shi Ah, qui travaille à Institut supérieur coréen des sciences et technologies (KAIST) comme archéologue va permettre de mettre en contact les deux époques, celle de Joseon et l'époque présente. Quand un dangereux assassin, Ma Dae Young (Song Dong II), est envoyé par la belle-mère de Joon Jae pour faire de son propre fils l’héritier de la fortune de Joon Jae, Shim Cheong pourra-t-elle survivre dans ce nouvel environnement bizarre tout en aidant Joon Jae à échapper aux dangers qui le guettent ? « La Légende de la Mer Bleue » est une série dramatique sud-coréenne de 2016-2017, réalisée par Jin Hyuk. L'histoire est inspirée d'une légende historique à propos d'une sirène, contenue dans une anthologie de nouvelles intitulée « Eou Yadam », de l'érudit de l'ère Joseon, Yu Mong In. Cette série marque le dernier rôle de la superstar Lee Min Ho avant son service militaire obligatoire de deux ans ainsi que le premier rôle de Jun Ji Hyun après la naissance de son premier enfant. Une partie de la série a été tournée sur l’île de Palaos, dans le Pacifique, et en Espagne.")
+   .setStory_fr_source(["Viki","https://www.viki.com/tv/32240c-the-legend-of-the-blue-sea"])
+   .setStory_en("How can a mermaid from the Joseon era survive in modern society?
 
+
+
+(`Shim Cheong (Jun Ji Hyun) is a mermaid who follows her one true love, a nobleman’s son named Kim Moon from the Joseon Dynasty, to modern-day Seoul. Moon’s modern doppelgänger is Heo Joon Jae (Lee Min Ho), a highly skilled scam artist who first becomes interested in Shim Cheong because of a jade bracelet worth $6 million that Shim Cheong wears.
+
+
+
+With nowhere to go, Shim Cheong is taken in by Joon Jae, who also finds his con partners, Jo Nam Doo (Lee Hee Joon) and Tae Oh (Shin Won Ho), hiding out in his home from a vengeful past victim of their scams. Shim Cheong discovers a rival for Joon Jae’s heart in Cha Shi Ah (Shin Hye Sun), a researcher at KAIST who deals with ancient artifacts.
+
+
+
+When a dangerous killer named Ma Dae Young (Sung Dong Il) sent by Joon Jae’s stepmother who wants to make her own son the heir to Joon Jae’s fortunes, can Shim Cheong survive her strange new environment while also helping Joon Jae avoid the dangers that await him?
+
+
+
+“The Legend of the Blue Sea” is a 2016-2017 South Korean drama series directed by Jin Hyuk. The story is inspired by a historical tale about a mermaid contained in a short story anthology known as Eou Yadam by Joseon-era scholar Yu Mong In.
+
+
+
+This drama marks the last acting role for superstar Lee Min Ho before he entered his mandatory two-year military service and the first acting role for Jun Ji Hyun after giving birth to her first child. Parts of the drama series were filmed on the Pacific island of Palau and in Spain.`)
+     .setStory_en_source(["Viki","https://www.viki.com/tv/32240c-the-legend-of-the-blue-sea"])
+     .setDrama_fr("https://www.viki.com/tv/32240c-the-legend-of-the-blue-sea?site_lang=fr")
+     .setDrama_en("https://www.viki.com/tv/32240c-the-legend-of-the-blue-sea?site_lang=en")
+     .setOp(["undefined"])
+     .setOpFull(["undefined"])
+     .setEd(["undefined"])
+     .setEdFull(["undefined"])
+     .addTo(listDrama1);
+                
+     new drama()
+.setName("Weightlifting Fairy kim bok joo ")
+    .setImage("https://www.nautiljon.com/images/drama/00/13/weightlifting_fairy_kim_bok_joo_2131.jpg")
+    .setStory_fr("Que pourrait-il y avoir d’autre que des haltères et des poids lourds pour vivre ? Kim Bok Joo (Lee Sung Kyung) est une haltérophile phénoménale qui se concentre uniquement sur les haltères en grandissant avec son père, Kim Chang Geol (Ahn Gil Kang), un ancien haltérophile. Elle fréquente l’école d’éducation physique Hanwool, une université pleine d’athlètes de premier ordre qui sont conduits à la victoire dans l’espoir de représenter leur pays dans les compétitions nationales et internationales. Son meilleur ami est Jeong Joon Hyeong (Nam Joo Hyuk), un nageur de compétition qui a des difficultés à se remettre du traumatisme d’avoir été disqualifié d’une compétition internationale de natation à cause d’un faux départ. Song Shi Ho (Kyung Soo Jin) est une compétitrice acharnée de gymnastique rythmique qui a gagné la médaille d'argent aux Jeux Asiatiques quand elle avait 18 ans, mais à cause de la pression de son sport elle a rompu avec Joon Hyeong. La vision étroite de la vie de Bok Joo commence à changer quand elle tombe amoureuse du cousin de Joon Hyeong, Jeong Jae Yi (Lee Jae Yoon), un ancien athlète qui est devenu médecin nutritionniste après avoir souffert d'une blessure qui a causé la fin de sa carrière. Bok Joo va-t-elle apprendre qu'il y a plus important que l'haltérophilie dans la vie ? \"Weightlifting Fairy Kim Bok Joo\" est un drama Coréen de 2016-2017 réalisé par Oh Hyun Jong. L'histoire s'inspire de la vie de l'haltérophile Jang Mi Ran, qui a gagné la meilleure d'or aux Jeux Olympiques d'été de 2008, aux Jeux Asiatiques Guangzhou de 2010 et a été 4 fois championne du monde. L'acteur populaire Lee Jong Suk fait une brève apparition dans le drama")
+    .setStory_fr_source(["Viki","https://www.viki.com/tv/32634c-weightlifting-fairy-kim-bok-joo"])
+    .setStory_en(`What else could there be to life than barbells and heavy weights?
+
+
+
+Kim Bok Joo (Lee Sung Kyung) is a weightlifting phenom who has only focused on barbells her entire life while growing up with her father, Kim Chang Gul (Ahn Gil Kang), is a former weightlifter. She attends Hanwool College of Physical Education, a university full of top-notch athletes who are driven to succeed in the hopes of representing their country in national and international competitions.
+
+
+
+Bok Joo went to the same elementary school as Jung Joon Hyung (Nam Joo Hyuk) but reunites with him in college. He is now a competitive swimmer who is having trouble recovering from the trauma of being disqualified for a false start in his first international swim competition.
+
+
+
+Song Shi Ho (Kyung Soo Jin) is a fiercely competitive rhythmic gymnast who won a silver medal at the Asian Games when she was 18, but the pressures of her sport drive her to break up with Joon Hyung. Bok Joo’s tunnel-vision life starts to change when she falls in love with Joon Hyung’s older cousin, Jung Jae Yi (Lee Jae Yoon), a former athlete who became an obesity doctor after suffering a career-ending injury.
+
+
+
+Will Bok Joo learn that there is more to life than weightlifting?
+
+
+
+“Weightlifting Fairy Kim Bok Joo” is a 2016-2017 South Korean drama series directed by Oh Hyun Jong. The story is inspired by the life of weightlifter Jang Mi Ran, who won the gold medal in the 2008 Summer Olympics, the gold medal in the 2010 Guangzhou Asian Games, and four world championships. Popular actor Lee Jong Suk makes a high-publicity cameo appearance in the drama.`)
+    .setStory_en_source(["Viki,https://www.viki.com/tv/32634c-weightlifting-fairy-kim-bok-joo?content_lang=en&site_lang=en"])
+    .setDrama_fr("https://www.viki.com/tv/32634c-weightlifting-fairy-kim-bok-joo?content_lang=en&site_lang=fr")
+    .setDrama_en("https://www.viki.com/tv/32634c-weightlifting-fairy-kim-bok-joo?content_lang=en&site_lang=en")
+    .setOp(["undefined"])
+    .setOpFull(["undefined"])
+    .setEd(["undefined"])
+    .setEdFull(["undefined"])
+    .addTo(listDrama1);
     
+    new drama()
+    .setName("Click your heart")
+    .setImage("http://dramavostfr.com/file/2017/04/clik.jpg")
+    .setStory_fr(`Personne ne peut résister à ses charmes maladroits. Kwon Mi Na (Kwon Mina) est une lycéenne dynamique qui n'arrête pas de se mettre dans des situations compliquées à cause de sa maladresse. Mais cela ne semble pas dissuader quatre garçons très différents de s'intéresser à elle.
+
+
+
+Kim Ro Woon (Ro Woon) est le lanceur vedette de baseball. Kim Da Won (Da Won) est le meilleur étudiant de l'école et une ami de longue date de Mi Na. Baek Ju Ho (Ju Ho) agit comme un voyou des rues, mais a un cœur d'or pour les personnes qui gagnent sa confiance. Kang Cha Ni (Cha Ni) est membre d'un boys band qui se passionne pour la danse. Qui arrivera à voler le cœur de Mi Na ?
+
+
+
+« Click Your Heart » est une web-série dramatique sud-coréenne où les spectateurs choisissent la fin de chaque épisode par le biais d'un vote.`)
+    .setStory_fr_source(["Viki","https://www.viki.com/tv/35631c-click-your-heart?content_lang=fr&q=click"])
+    .setStory_en(`They can’t resist her clumsy charms. Kwon Mi Na (Kwon Mina) is a peppy high school girl who keeps getting into difficult situations because of her clumsiness. But that doesn’t seem to deter four very different boys from taking an interest in her.
+
+
+
+Kim Ro Woon (Ro Woon) is the star baseball pitcher. Kim Da Won (Da Won) is the school’s top student and a longtime friend of Mi Na’s. Baek Ju Ho (Ju Ho) acts like a cold street thug but has a heart of gold for people who earn his trust. Kang Cha Ni (Cha Ni) is a boy band member who is passionate about dancing. Which boy will capture Mi Na’s heart?
+
+
+
+“Click Your Heart” is a 2016 South Korean web drama series where the viewers got to choose the ending of each episode through their votes.`)
+    .setStory_en_source(["Viki","https://www.viki.com/tv/35631c-click-your-heart?content_lang=en&q=click"])
+    .setDrama_fr("https://www.viki.com/tv/35631c-click-your-heart?content_lang=fr&q=click")
+    .setDrama_en("https://www.viki.com/tv/35631c-click-your-heart?content_lang=en&q=click")
+    .setOp(["undefined"])
+    .setOpFull(["undefined"])
+    .setEd(["undefined"])
+    .setEdFull(["undefined"])
+    .addTo(listDrama1);
+             
 exports.load = () => {
     return listDrama = listDrama1.dramas
 }
+
